997868f8843ac3d66e5b7f6d4e51ef89ff555c4c
William C. Johnson <wcjohnson@oigroup.net>
2018-01-03 11:05:12 -0500

    [es] Packaging fixes

--- elasticsearch/rollup.config.js
+++ elasticsearch/rollup.config.js
@@ -6,18 +6,35 @@ var path = require('path');
 var srcPath = (path.resolve('./src')).replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
 var srcPathRegex = new RegExp(srcPath)
 
-var coverage = false;
-if (process.env.COVERAGE === "true") coverage = true;
-
 // Disable module transform when building for rollup
 var babelRC = JSON.parse(fs.readFileSync('./.babelrc', { encoding: 'UTF-8'}))
 babelRC.babelrc = false;
 babelRC.presets[0][1].env.modules = false
 
+// Add babel-external-helpers plugin, which helps rollup dedupe injected code
+babelRC.presets[0][1].additionalPlugins = babelRC.presets[0][1].additionalPlugins || [];
+babelRC.presets[0][1].additionalPlugins.push("babel-plugin-external-helpers");
+
+// Istanbul code coverage
+var coverage = false;
+if (process.env.COVERAGE === "true") coverage = true;
 if(coverage) {
-  // Add Istanbul code coverage
-  babelRC.presets[0][1].additionalPlugins = babelRC.presets[0][1].additionalPlugins || [];
   babelRC.presets[0][1].additionalPlugins.push("babel-plugin-istanbul");
+}
+
+// Attempt to determine if a module is external and should not be rolled into
+// the bundle. Check for presence in source path, presence of "." in module path,
+// or special module paths.
+function isExternal(modulePath) {
+  // "babelHelpers" must be treated as internal or babel-plugin-external-helpers will break
+  if(/babelHelpers/.test(modulePath)) return false;
+
+  // "." in module path = internal
+  if(/\.\//.test(modulePath)) return false;
+
+  // Otherwise, attempt to figure out whether the module is inside the source tree.
+  modulePath = path.resolve(modulePath)
+  return !(srcPathRegex.test(modulePath));
 }
 
 var getPlugins = () => [
@@ -30,17 +47,12 @@ var getPlugins = () => [
 var withFormat = (format) => ({
   input: 'src/index.lsc',
   output: {
-    file: `lib/index.${format}.js`,
+    file: format === "cjs" ? `lib/index.js` : `lib/index.${format}.js`,
     format: format
   },
   sourcemap: 'inline',
   plugins: getPlugins(),
-  external: (modulePath) => {
-    if(/\.\//.test(modulePath)) return false;
-    modulePath = path.resolve(modulePath)
-    var rst = !(srcPathRegex.test(modulePath));
-    return rst;
-  }
+  external: isExternal
 })
 
 // Only build CJS when doing coverage
